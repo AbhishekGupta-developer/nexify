8c922a5f33c717782271bc4c1cdb18e9b65f8162
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-11-10 15:51:52 +0530

    Add User response DTO

---
+++ src/main/java/com/myorganisation/nexify/dto/response/UserResponseDto.java
@@ -0,0 +1,13 @@
+package com.myorganisation.nexify.dto.response;
+
+import com.myorganisation.nexify.enums.Gender;
+import lombok.Data;
+
+@Data
+public class UserResponseDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String username;
+    private Gender gender;
+}
