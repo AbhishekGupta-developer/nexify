67c1aba087cbb651118431fcff4f8065d2f0a996
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-11-10 15:56:10 +0530

    Add User service interface

---
+++ src/main/java/com/myorganisation/nexify/service/UserService.java
@@ -0,0 +1,15 @@
+package com.myorganisation.nexify.service;
+
+import com.myorganisation.nexify.dto.request.UserRequestDto;
+import com.myorganisation.nexify.dto.response.GenericResponseDto;
+import com.myorganisation.nexify.dto.response.UserResponseDto;
+
+import java.util.List;
+
+public interface UserService {
+    UserResponseDto registerUser(UserRequestDto userRequestDto);
+    UserResponseDto getUser(Long id);
+    List<UserResponseDto> getAllUsers();
+    UserResponseDto updateUser(Long id, UserRequestDto userRequestDto);
+    GenericResponseDto removeUser(Long id);
+}
