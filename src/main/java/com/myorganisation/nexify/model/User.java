a608bb77dc389c2638457a9961ac6a281eab9221
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-11-18 20:54:30 +0530

    Convert it into actual entity

--- src/main/java/com/myorganisation/nexify/model/User.java
+++ src/main/java/com/myorganisation/nexify/model/User.java
@@ -1,11 +1,17 @@
 package com.myorganisation.nexify.model;
 
 import com.myorganisation.nexify.enums.Gender;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
     private String username;
