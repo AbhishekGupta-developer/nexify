b55855056c5d300f13164d55d8c3a926c7f8a138
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-11-20 16:28:21 +0530

    Add custom finder methods

--- src/main/java/com/myorganisation/nexify/repository/UserRepository.java
+++ src/main/java/com/myorganisation/nexify/repository/UserRepository.java
@@ -4,6 +4,11 @@ import com.myorganisation.nexify.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    // Custom finder methods
+    User findByUsername(String username);
+    List<User> findByUsernameContaining(String username);
 }
