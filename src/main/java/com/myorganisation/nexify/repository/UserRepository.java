ac960c2dac8d8959030b91f5c48e503d12739b87
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-12-02 14:00:09 +0530

    Add JPQL method

--- src/main/java/com/myorganisation/nexify/repository/UserRepository.java
+++ src/main/java/com/myorganisation/nexify/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package com.myorganisation.nexify.repository;
 
+import com.myorganisation.nexify.enums.Gender;
 import com.myorganisation.nexify.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +15,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
     List<User> findByUsernameContaining(String username);
     List<User> findByName(String name);
+
+    // JPQL
+    @Query("SELECT u FROM User u WHERE u.name = :name AND u.gender = :gender")
+    List<User> searchByNameAndGender(@Param("name") String name, @Param("gender") Gender gender);
 }
