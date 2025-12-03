2b6c6c38400f4f657c0238f6aa4601742fc83651
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-12-03 20:25:12 +0530

    Add Profile entity

---
+++ src/main/java/com/myorganisation/nexify/model/Profile.java
@@ -0,0 +1,24 @@
+package com.myorganisation.nexify.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "profiles")
+public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String bio;
+    private String avatarUrl;
+    private String displayName;
+    private Date dob;
+
+    @OneToOne(mappedBy = "profile")
+//    @JoinColumn(name = "user")
+    private User user;
+}
