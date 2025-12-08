d14b83baccbbaa1a151ac683e23e0b6d58655435
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-12-08 20:29:14 +0530

    Add Profile and MetaData in Response DTO

--- src/main/java/com/myorganisation/nexify/dto/response/UserResponseDto.java
+++ src/main/java/com/myorganisation/nexify/dto/response/UserResponseDto.java
@@ -1,6 +1,8 @@
 package com.myorganisation.nexify.dto.response;
 
 import com.myorganisation.nexify.enums.Gender;
+import com.myorganisation.nexify.model.MetaData;
+import com.myorganisation.nexify.model.Profile;
 import lombok.Data;
 
 @Data
@@ -10,4 +12,6 @@ public class UserResponseDto {
     private String email;
     private String username;
     private Gender gender;
+    private Profile profile;
+    private MetaData metaData;
 }
