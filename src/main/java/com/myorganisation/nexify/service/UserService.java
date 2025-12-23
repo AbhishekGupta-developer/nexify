ce67f567c6fce0cf217a214066787561e5059e43
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-12-23 20:12:40 +0530

    Add getUserPage method

--- src/main/java/com/myorganisation/nexify/service/UserService.java
+++ src/main/java/com/myorganisation/nexify/service/UserService.java
@@ -4,6 +4,8 @@ import com.myorganisation.nexify.dto.request.UserRequestDto;
 import com.myorganisation.nexify.dto.response.GenericResponseDto;
 import com.myorganisation.nexify.dto.response.UserResponseDto;
 import com.myorganisation.nexify.enums.Gender;
+import com.myorganisation.nexify.model.User;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -20,4 +22,6 @@ public interface UserService {
     List<UserResponseDto> getUsersByName(String name);
 
     List<UserResponseDto> searchByNameAndGender(String name, Gender gender, String type);
+
+    Page<UserResponseDto> getUserPage(Integer pageIndex, Integer pageSize, String sortBy, String sortOrder);
 }
