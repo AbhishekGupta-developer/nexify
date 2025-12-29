e3d82f4e27d5788a94cbe72e6bd26361b245de81
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-12-29 20:04:53 +0530

    Add Cron job

---
+++ src/main/java/com/myorganisation/nexify/cron/SendHelloCron.java
@@ -0,0 +1,24 @@
+package com.myorganisation.nexify.cron;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SendHelloCron {
+
+    @Scheduled(cron = "* * * * * ?")
+    public void sayHelloEverySecond() {
+        System.out.println("Hello");
+    }
+
+    @Scheduled(cron = "0 * * * * ?")
+    public void sayHiEveryMinute() {
+        System.out.println("Hi");
+    }
+
+    @Scheduled(cron = "*/30 * * * * ?")
+    public void sayNamasteTwoTimesEveryMinute() {
+        System.out.println("Namaste");
+    }
+}
+
