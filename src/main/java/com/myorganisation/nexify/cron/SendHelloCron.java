3ed6dbaaf82562e0ee0df1f0552fcaeedbb51ad4
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-12-30 12:41:15 +0530

    Add new cron job and comment existing jobs

--- src/main/java/com/myorganisation/nexify/cron/SendHelloCron.java
+++ src/main/java/com/myorganisation/nexify/cron/SendHelloCron.java
@@ -6,19 +6,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class SendHelloCron {
 
-    @Scheduled(cron = "* * * * * ?")
-    public void sayHelloEverySecond() {
-        System.out.println("Hello");
-    }
+//    @Scheduled(cron = "* * * * * ?")
+//    public void sayHelloEverySecond() {
+//        System.out.println("Hello");
+//    }
+//
+//    @Scheduled(cron = "0 * * * * ?")
+//    public void sayHiEveryMinute() {
+//        System.out.println("Hi");
+//    }
+//
+//    @Scheduled(cron = "*/30 * * * * ?")
+//    public void sayNamasteTwoTimesEveryMinute() {
+//        System.out.println("Namaste");
+//    }
 
-    @Scheduled(cron = "0 * * * * ?")
-    public void sayHiEveryMinute() {
-        System.out.println("Hi");
-    }
-
-    @Scheduled(cron = "*/30 * * * * ?")
-    public void sayNamasteTwoTimesEveryMinute() {
-        System.out.println("Namaste");
+    @Scheduled(cron = "${my.cron.job}")
+    public void performJobAccordingToEnvironment() {
+        System.out.println("Environment");
     }
 }
 
