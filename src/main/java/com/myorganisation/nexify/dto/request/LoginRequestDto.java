502f159f2b117077d7297ecfa9c06a5d1ef2a4e5
Abhishek Gupta <abhishekg.developer@gmail.com>
2026-01-27 19:38:48 +0530

    Add Login request DTO

---
+++ src/main/java/com/myorganisation/nexify/dto/request/LoginRequestDto.java
@@ -0,0 +1,11 @@
+package com.myorganisation.nexify.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequestDto {
+    private String username;
+    private String password;
+}
